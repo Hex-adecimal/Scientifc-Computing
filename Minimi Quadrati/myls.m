64ee6060193351519a455074e8106127917da500
Luigi Penza <91316353+Hex-adecimal@users.noreply.github.com>
2021-11-21 23:11:49 +0100

    Add files via upload

--- Minimi Quadrati/myls.m
+++ Minimi Quadrati/myls.m
@@ -1,10 +1,28 @@
 function coeff = myls(xdata, ydata, m)
+    % coeff = myls(xdata, ydata, m)
+    % E' una funzione che prende in INPUT due vettori di dimensione n
+    % ed il numero di coefficienti che vogliamo in output
+    % Restituisce in OUTPUT m coefficienti di un polinomio 
+    %
+    % Questa funzione usa il metodo dei minimi quadrati per trovare dei
+    % coefficienti di un polinomio che approssima i nodi
+    %
+    % Nello specifico, viene costruita una matrice dei coefficienti B che
+    % contiene le potenze dei nodi x da x^0 a x^m. Viene costruito un
+    % vettore di termini noti b che contiene la somma degli y_i
+    % moltiplicati per le potenze di x_i. Viene risolto questo sistema, ed
+    % il risultato è proprio il vettore dei coefficienti del polinomio.
+    % 
     % xdata : vettore riga : sono le ascisse dei punti dati
     % ydata : vettore riga : sono le ordinate dei punti dati
     % m : intero : è il numero di coefficienti del polinomio
     % Questa funzione restituisce in ordine i coefficienti a_0, ..., a_m;
     % Non c'è alcun controllo sull'input;
-    % Si basa sul metodo dei minimi quadrati;
+    
+    if length(xdata) ~= length(ydata)
+        disp('La dimensione dei dati deve essere uguale!')
+        return
+    end
     
     m = m-1;
     n = length(xdata) - 1;
