fbe0b836379f865d340785d748b0ada9cf1dc890
Luigi Penza <91316353+Hex-adecimal@users.noreply.github.com>
2021-12-26 20:06:19 +0100

    Add files via upload

---
+++ Equazioni differenziali/myEuler.m
@@ -0,0 +1,29 @@
+function [t, y] = myEuler(fname, y0, t0, T, N)
+    % .
+    %
+    % .
+    %
+    % INPUT
+    %   fname : La funzione f(t, y(t))
+    %   y0    : Il valore iniziale
+    %   t0, T : Gli estremi dell'intervallo di integrazione
+    %   N     : Il numero di intervallini in cui suddividere [t0, T]
+    % OUTPUT
+    %   t     : L'insieme di rete t0, t1, ..., tN
+    %   y     : L'approssimazione della soluzione nei punti di rete
+    
+    if nargin < 5 
+        return 
+    end
+    
+    h = (T-t0)/N; % Passo di discretizzazione
+    t = t0:h:T;   % Intervallo di integrazione
+    
+    % Inizializzo y
+    y = zeros(1, N); y(1) = y0; 
+    
+    for k = 1:N
+        y(k+1) = y(k) + h*fname(t(k), y(k)); % Eseguo un passo
+    end
+    
+end
